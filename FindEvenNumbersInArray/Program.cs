1b5475571e17439598ffda8f2d6d7812fa74c292
Yakov Krasner <yakov.krasner@yandex.ru>
2022-08-26 16:21:55 +0700

    Initial commit_Find even numbers in array

---
+++ FindEvenNumbersInArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int lowerBoundOfRandom = 100;
+int upperBoundOfRandom = 999;
+int NumbersCount = 6;
+int [] array = new int [NumbersCount];  
+int CountOfEvenNumbers = 0;
+for (int Index = 0; Index<array.Length; Index++)
+    {
+        array [Index] = new Random().Next(lowerBoundOfRandom,upperBoundOfRandom+1); 
+    }
+for (int Index = 0; Index<array.Length; Index++)
+    {
+       if (array [Index] % 2 == 0) CountOfEvenNumbers ++;
+    }
+
+Console.WriteLine (CountOfEvenNumbers);
