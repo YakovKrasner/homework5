5a423e4f9a201e3e98e195d005ff68d5e7c7c81b
Yakov Krasner <yakov.krasner@yandex.ru>
2022-08-26 16:59:05 +0700

    Initial commit_Find difference between max and min elements in array

---
+++ FindDifferenceOfMinAndMax/Program.cs
@@ -0,0 +1,18 @@
+﻿double upperBoundOfRandom = 50.0;
+int NumbersCount = 6;
+double [] array = new double [NumbersCount];  
+
+for (int Index = 0; Index<array.Length; Index++)
+    {
+        array [Index] = new Random().NextDouble()*upperBoundOfRandom; 
+        Console.WriteLine (array[Index]);
+    }
+double Max = array [0];
+double Min = array [0];
+for (int Index = 0; Index<array.Length; Index++)
+    {
+        if (array[Index] < Min) Min = array[Index];
+        if (array[Index] > Max) Max = array[Index];
+    }
+
+Console.WriteLine ($"Разность между максимальным и минимальным элементами массива равна {Max - Min}"); 
