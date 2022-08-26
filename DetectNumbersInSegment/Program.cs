ce32369a509aa99a4211ca2bd1b43600f37959c3
Yakov Krasner <yakov.krasner@yandex.ru>
2022-08-26 15:07:16 +0700

    Initial commit_Detect numbers in segment

---
+++ DetectNumbersInSegment/Program.cs
@@ -0,0 +1,21 @@
+﻿int NumbersCount = 123; // указываем количество чисел
+int lowerBoundOfRandom = 1;
+int upperBoundOfRandom = 300; // Указываем верхнюю и нижнюю границы отрезка в рамках которого будут генерироваться псевдолучайные числа 
+int lowerBoundOfDetection = 10; 
+int upperBoundOfDetection = 99; // Указываем верхнюю и нижнюю границы отрезка в рамках которого будем искать количество принадлежащих ему чисел 
+int NumbersBetweenCount = 0;
+int [] array = new int [NumbersCount];  // Создаем массив из NumbersCount элементов, затем заполняем его псевдослучайными числами
+for (int Index = 0; Index<array.Length; Index++)
+    {
+        array [Index] = new Random().Next(lowerBoundOfRandom,upperBoundOfRandom+1); 
+    }
+
+for (int Index = 0; Index<array.Length; Index++)
+    {
+        if (array [Index] >=  lowerBoundOfDetection && array [Index] <= upperBoundOfDetection) // Проверяем псевдослучайные числа из массива Array на принадлежность отрезку [lowerBoundOfDetection,upperBoundOfDetection]
+        {
+            NumbersBetweenCount ++;
+        }
+    }
+Console.WriteLine ();
+Console.WriteLine (NumbersBetweenCount);
