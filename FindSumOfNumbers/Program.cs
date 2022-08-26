4cdcba072c3dfc35a402ce30a5204432f5ab423f
Yakov Krasner <yakov.krasner@yandex.ru>
2022-08-26 16:05:10 +0700

    Initial commit_Find sum of pairs

---
+++ FindSumOfNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿int CountOfNumbers = 9;
+int [] array = new int [CountOfNumbers];
+int lowerBoundOfRandom = 1;
+int upperBoundOfRandom = 10; // Указываем верхнюю и нижнюю границы отрезка в рамках которого будут генерироваться псевдолучайные числа 
+
+for (int index = 0; index<array.Length; index ++)
+    { 
+        array[index] = new Random().Next(lowerBoundOfRandom,upperBoundOfRandom+1);
+        Console.Write ($" {array[index]}");
+    }
+Console.WriteLine ();
+for (int index = 0; index<=((array.Length -1) - index); index ++)
+    {
+        if (index == ((array.Length - 1) - index)) Console.Write ($" {array[index]}");
+        else Console.Write ($" {array[index] + array[(array.Length - 1) - index]}");
+    }
+Console.WriteLine ();
