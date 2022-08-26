75cf885ca78e63c7cf18b2a80db37ba5404c92c0
Yakov Krasner <yakov.krasner@yandex.ru>
2022-08-26 16:28:39 +0700

    Initial commit_Find sum of elements in uneven positions

---
+++ FindSumInUnevenPositions/Program.cs
@@ -0,0 +1,16 @@
+﻿int lowerBoundOfRandom = -10;
+int upperBoundOfRandom = 50;
+int NumbersCount = 6;
+int [] array = new int [NumbersCount];  
+int sumOfUneven = 0;
+for (int Index = 0; Index<array.Length; Index++)
+    {
+        array [Index] = new Random().Next(lowerBoundOfRandom,upperBoundOfRandom+1); 
+        Console.WriteLine (array[Index]);
+    }
+for (int Index = 0; Index<array.Length; Index++)
+    {
+       if (Index % 2 == 1) sumOfUneven = sumOfUneven + array[Index];
+    }
+
+Console.WriteLine (sumOfUneven);
